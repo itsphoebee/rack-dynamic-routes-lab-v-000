8151649cb6c8358ac0c2e1e8ad2a7fb3961f5d9e
Phoebe Lau <phoebelau7@gmail.com>
2018-01-05 20:41:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,13 +5,12 @@ class Application
 
     if req.path.match(/items/)
       item_name= req.path.split("/items/").last
-      if @@items.find {|i| i.name == item_name
+      @@items.find {|i| if i.name == item_name
         resp.status = 200
         resp.write i.price
       else
         resp.write "Item not found"
         resp.status = 400
-      }
       end
     else
       resp.write "Route not found"
